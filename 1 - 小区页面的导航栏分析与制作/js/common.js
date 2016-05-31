b42f5e8e8f060241488b9c1ad1f7d2bac41f4bc8
chennengbao <718747239@qq.com>
2016-05-31 09:31:16 +0800

    小区页面的导航分析与制作

---
+++ 1 - 小区页面的导航栏分析与制作/js/common.js
@@ -0,0 +1,30 @@
+/* 
+* @Author: 陈能堡 - 梦幻雪冰
+* @Date:   2015-12-15 21:42:22
+* @Last Modified by:   陈能堡 - 梦幻雪冰
+* @Last Modified time: 2016-05-31 09:22:41
+*/
+!function() {
+    var win     = window,
+        timer   = null;
+
+    function handlerLayout() {
+        var comHeader = $(".com-header"),
+            comFooter = $(".com-footer"),
+            layoutTop = $(".layout-top"),
+            layoutBot = $(".layout-bot");
+
+        layoutTop.css("height", comHeader.height());
+        layoutBot.css("height", comFooter.height());
+        
+    }handlerLayout();
+
+    win.addEventListener("resize", function() {
+        clearTimeout(timer),
+        timer = setTimeout(handlerLayout, 300);
+    }, !1),
+    // onload 事件在页面第一次加载时触发， onpageshow 事件在每次加载页面时触发，即 onload 事件在页面从浏览器缓存中读取时不触发。
+    win.addEventListener("pageshow", function(a) {
+        win.persisted && (clearTimeout(timer), timer = setTimeout(handlerLayout, 300));
+    }, !1)
+}();
